def7fdcd5575c1828ca909e1d32f3f0bc4e6bfd1
Fabio Berger <me@fabioberger.com>
2017-10-13 23:14:37 +0300

    Fix call to action

--- ts/pages/landing/landing.tsx
+++ ts/pages/landing/landing.tsx
@@ -805,7 +805,7 @@ export class Landing extends React.Component<LandingProps, LandingState> {
             lineHeight: '33px',
             height: 49,
         };
-        const callToActionClassNames = 'col lg-col-8 md-col-8 col-12 lg-pr4 md-pr4 \
+        const callToActionClassNames = 'col lg-col-8 md-col-8 col-12 lg-pr3 md-pr3 \
                                         lg-right-align md-right-align sm-center sm-px3 h4';
         return (
             <div
@@ -819,7 +819,7 @@ export class Landing extends React.Component<LandingProps, LandingState> {
                         className={callToActionClassNames}
                         style={{fontFamily: 'Roboto Mono', color: 'white', lineHeight: isSmallScreen ? 1.7 : 3}}
                     >
-                        Get started building the decentralized future
+                        Get started on building the decentralized future
                     </div>
                     <div className="col lg-col-4 md-col-4 col-12 sm-center sm-pt2">
                         <Link to={WebsitePaths.ZeroExJs} className="text-decoration-none">
